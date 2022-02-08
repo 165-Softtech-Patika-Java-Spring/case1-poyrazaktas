2500d41910fb6ec00392c258822bce7e1cdd294e
Poyraz Aktas <poyraz.educational@gmail.com>
2022-02-08 11:37:38 +0300

    add house types

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import house.House;
+import house.SummerHouse;
+import house.Villa;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        House house = new House(1_100_400L, 200, 4, 1);
+        House house2 = new Villa(2_100_400L, 300, 6, 2);
+        House house3 = new SummerHouse(100_400L, 100, 3, 1);
+
     }
 }
---
+++ src/house/Villa.java
@@ -0,0 +1,8 @@
+package house;
+
+public class Villa extends House{
+
+    public Villa(long price, int squareMeter, int numberOfRooms, int numberOfLivingRooms) {
+        super(price, squareMeter, numberOfRooms, numberOfLivingRooms);
+    }
+}
